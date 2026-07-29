8391a15c38a4fbde923fc740bde8b72563a3d03a
326974_nttds <326974@nttdata.com>
2026-07-29 14:12:36 -0500

    Initial Commit

---
+++ MySwiftyRepo/MySwiftyRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftyRepoApp.swift
+//  MySwiftyRepo
+//
+//  Created by 326974 on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftyRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
